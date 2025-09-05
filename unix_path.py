99df756ffdbaef36f37e1f13ca24eb4bcdff5f7a
medegor44 <medegor44@gmail.com>
2025-09-05 14:32:50 +0300

    Implement isomorphic strings solution and add path simplification function

---
+++ unix_path.py
@@ -0,0 +1,19 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path_parts = path.split('/')
+        stack = []
+
+        for x in path_parts:
+            match (x):
+                case '.':
+                    continue
+                case '..':
+                    if len(stack) > 0:
+                        stack.pop()
+                case '':
+                    continue
+                case _:
+                    stack.append(x)
+        return f"/{str.join('/', stack)}" 
+    
+print(Solution().simplifyPath("/.../a/../b/c/../d/./"))
